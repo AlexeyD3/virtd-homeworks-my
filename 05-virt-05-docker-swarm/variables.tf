0bbdd0d0d444f96b0028c2664bda3fd4c06a95fa
AlexeyD3 <111375168+AlexeyD3@users.noreply.github.com>
2023-09-18 14:26:35 +0300

    Add files via upload

---
+++ 05-virt-05-docker-swarm/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gm5cnlfgcnmeq4an1v"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1ggvhbs15psoc1vgtkc"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8dug9jb3hl8fbv0na8"
+}
